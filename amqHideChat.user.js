7ae057f7e6d4c642f031523de194540dbe0836bb
Son Pham <son061203@gmail.com>
2025-08-03 14:40:47 +0700

    update amqHideChat to 1.1

--- amqHideChat.user.js
+++ amqHideChat.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         AMQ Hide Chat
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.1
 // @description  Toggle hide non-team messages in AMQ
 // @author       peashooter and Github Copilot
 // @match        https://animemusicquiz.com/*
