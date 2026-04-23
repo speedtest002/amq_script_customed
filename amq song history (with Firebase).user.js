e15dc03cb3e09a86c85d8302bf437146a35eeb7e
Son Pham Hoang <70208849+speedtest002@users.noreply.github.com>
2026-04-23 17:59:26 +0700

    fix: track songs correctly when 720p link is missing

--- amq song history (with Firebase).user.js
+++ amq song history (with Firebase).user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         amq song history (with Firebase)
 // @namespace    http://tampermonkey.net/
-// @version      2.3
+// @version      2.3.1
 // @description  Display Song history in the song info box, including the guess rate and time since last time the song played. Synced with Firebase.
 // @author       Minigamer42 (modified by peashooter)
 // @match        https://animemusicquiz.com/*
@@ -76,7 +76,7 @@ If you used the old localStorage version before, run this in F12 Console to migr
 
 ============================================ */
 
-const version = "2.3";
+const version = "2.3.1";
 const infoDiv = document.createElement('div');
 infoDiv.className = "rowPlayCount";
 infoDiv.style.marginBottom = "10px";
@@ -297,7 +297,7 @@ function setup(database) {
     const l = new Listener("answer results");
     l.callback = async (data) => {
         const currentAns = getCurrentAns(data.players);
-        const webm = data.songInfo.videoTargetMap?.catbox?.[720]?.slice(0, 6) ?? data.songInfo.videoTargetMap?.catbox?.[480]?.slice(0, 6);
+        const webm = data.songInfo.videoTargetMap?.catbox?.[720]?.slice(0, 6) || data.songInfo.videoTargetMap?.catbox?.[480]?.slice(0, 6);
         if (!webm) {
             infoDiv.innerHTML = '';
             return;
